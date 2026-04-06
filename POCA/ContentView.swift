afa811c03529bc16ac3f076495209c92d009fcef
visualog <taikyong@gmail.com>
2026-04-06 22:01:28 +0900

    Initial Commit

---
+++ POCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POCA
+//
+//  Created by visualog on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POCA/POCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCAApp.swift
+//  POCA
+//
+//  Created by visualog on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct POCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
